c8be1571619efebe9cdd3a8444edbc5e929a6d55
GiG <g.gargaro@gmail.com>
2025-09-09 19:34:46 +0200

    Update output.tf

--- examples/satellite-vmware/modules/ibm-vmware-solutions-shared-instance/output.tf
+++ examples/satellite-vmware/modules/ibm-vmware-solutions-shared-instance/output.tf
@@ -1,33 +1,33 @@
 output "edge_gateway_name" {
-  value = data.vcd_edgegateway.edge.name
+  value = data.vcd_nst_edgegateway.edge.name
 }
 
 output "edge_gateway_id" {
-  value = data.vcd_edgegateway.edge.id
+  value = data.vcd_nst_edgegateway.edge.id
 }
 
 output "external_network_name_1" {
-  value = element([for net in data.vcd_edgegateway.edge.external_network : net.name if !tolist(net.subnet)[0].use_for_default_route], 0)
+  value = element([for net in data.vcd_nst_edgegateway.edge.external_network : net.name if !tolist(net.subnet)[0].use_for_default_route], 0)
 }
 
 output "external_network_name_2" {
-  value = element([for net in data.vcd_edgegateway.edge.external_network : net.name if tolist(net.subnet)[0].use_for_default_route], 0)
+  value = element([for net in data.vcd_nst_edgegateway.edge.external_network : net.name if tolist(net.subnet)[0].use_for_default_route], 0)
 }
 
 output "default_external_network_ip" {
-  value = data.vcd_edgegateway.edge.default_external_network_ip
+  value = data.vcd_nst_edgegateway.edge.default_external_network_ip
 }
 
 output "external_network_ips" {
-  value = tolist(data.vcd_edgegateway.edge.external_network_ips)
+  value = tolist(data.vcd_nst_edgegateway.edge.external_network_ips)
 }
 
 output "external_network_ips_1" {
-  value = tolist(data.vcd_edgegateway.edge.external_network_ips)[0]
+  value = tolist(data.vcd_nst_edgegateway.edge.external_network_ips)[0]
 }
 
 output "external_network_ips_2" {
-  value = tolist(data.vcd_edgegateway.edge.external_network_ips)[1]
+  value = tolist(data.vcd_nst_edgegateway.edge.external_network_ips)[1]
 }
 
 # added things
